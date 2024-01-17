8c928e9a97d6ad53c9fe36c236f87998bb25deb5
pk27734 <pk27734@users.noreply.github.com>
2024-01-17 11:43:51 +0100

    Initial commit with first version of codebase

---
+++ report.md
@@ -0,0 +1,32 @@
+# Robot Framework Report
+
+Total tests: 24
+:green_circle: 21 passed
+:red_circle: 3 failed
+
+|Test|Result|
+|---|---|
+|TC3a - Doctor Can Login As Admin User| :green_circle:|
+|TC3b - Pharmacist Can Login As Admin User| :green_circle:|
+|TC3c - Nurse Can Login As Normal User| :green_circle:|
+|TC3d - Physician Assistent Can Login As Normal User| :green_circle:|
+|TC3e - Kaakchirurg Cannot Login| :green_circle:|
+|TC3f - Fysiotherapeut Cannot Login| :green_circle:|
+|TC3g - Non-existing BRBA User| :green_circle:|
+|TC3h - User With Multiple Roles| :green_circle:|
+|TC9a - Device Test - iPhone 13| :green_circle:|
+|TC9b - Device Test - iPhone 13 Pro| :green_circle:|
+|TC9c - Device Test - iPhone 13 Mini| :green_circle:|
+|TC9d - Device Test - Pixel 5| :green_circle:|
+|TC9e - Device Test - Desktop Chrome| :green_circle:|
+|TC9f - Device Test - Desktop Safari| :green_circle:|
+|TC9g - Device Test - Desktop Firefox| :green_circle:|
+|TC9h - Device Test - Desktop Edge| :green_circle:|
+|Testidentiteit 1 Can Login| :green_circle:|
+|Testidentiteit 2 Can Login| :green_circle:|
+|TC4a - Login Not Allowed As Normal User1| :green_circle:|
+|TC4b - Login Allowed As Normal User2| :green_circle:|
+|Verify QR Code| :green_circle:|
+|Testidentiteit 3 Can Login| :red_circle:|
+|Testidentiteit 4 Can Login| :red_circle:|
+|Smoke test| :red_circle:|
---
+++ result_report.py
@@ -0,0 +1,42 @@
+from robot.api import ExecutionResult, ResultVisitor
+import sys
+
+class ResultReport(ResultVisitor):
+    def __init__(self, markdown_file='report.md'):
+        self.failed_tests = []
+        self.passed_tests = []
+        self.markdown_file = markdown_file
+
+    def visit_test(self, test):
+        if test.status == 'FAIL':
+            self.failed_tests.append(test.name)
+        elif test.status == 'PASS':
+            self.passed_tests.append(test.name)
+
+    def end_result(self, result):
+        # Create a new markdown file
+        with open(self.markdown_file, "w") as f:
+            f.write("# Robot Framework Report\n")
+            f.write("\n")
+            f.write("Total tests: " + str(len(self.passed_tests) + len(self.failed_tests)) + "\n")
+            f.write(":green_circle: " + str(len(self.passed_tests)) + " passed\n") 
+            f.write(":red_circle: " + str(len(self.failed_tests)) + " failed\n")
+            f.write("\n")
+            f.write("|Test|Result|\n")
+            f.write("|---|---|\n")
+            for test in self.passed_tests:
+                f.write(f"|{test}| :green_circle:|\n")
+            for test in self.failed_tests:
+                f.write(f"|{test}| :red_circle:|\n")
+
+if __name__ == '__main__':
+    try:
+        output_file = sys.argv[1]
+    except IndexError:
+        output_file = "output.xml"
+    try:
+        markdown_file = sys.argv[2]
+    except IndexError:
+        markdown_file = "report.md"
+    result = ExecutionResult(output_file)
+    result.visit(ResultReport())
