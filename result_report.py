9fff313d3dc9e52c357a7f26878c6642309ecfe5
pk27734 <pk27734@users.noreply.github.com>
2024-01-24 10:54:37 +0100

    Temp fix for warning, will fix in other branch

--- result_report.py
+++ result_report.py
@@ -18,7 +18,8 @@ class ResultReport(ResultVisitor):
         elif test.status == 'PASS':
             self.passed_tests.append(test.name)
 
-    def end_result(self):
+    
+    def end_result(self, result): # pylint: disable=W0621, W0613
         """Implementation of end_result"""
         # Create a new markdown file
         with open(self.markdown_file, "w", encoding="utf-8") as f:
